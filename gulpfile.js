f0b745a15be2b5491e64f9837e14cd8660e5d98e
Serednii Mykola <seredniimykola@gmail.com>
2024-05-03 00:53:49 +0200

    add babel

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 const del = require('del');
+const babel = require('gulp-babel');
 
 const sync = require('browser-sync').create();
 
@@ -55,6 +56,9 @@ function scripts() {
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
     .pipe(uglify()) // мініиізує файл
     .pipe(dest('app/js'))
     .pipe(sync.stream())
